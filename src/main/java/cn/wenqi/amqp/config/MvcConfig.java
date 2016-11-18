f433ac323e0cb1baaff345a4fec17eeed2508629
goahead77 <736694675@qq.com>
2016-11-18 17:34:49 +0800

    add

---
+++ src/main/java/cn/wenqi/amqp/config/MvcConfig.java
@@ -0,0 +1,31 @@
+package cn.wenqi.amqp.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * @author wenqi
+ */
+
+@SuppressWarnings("SpringJavaAutowiringInspection")
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+}
